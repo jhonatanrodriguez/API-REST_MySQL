c23d89fe9a6248f31d17b89c6d97ebf0d81cef3b
jhonatan rodriguez <48069972+jhonatanrodriguez@users.noreply.github.com>
2023-04-20 22:51:52 -0500

    Delete index.js

--- Order/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express()
-const orderRouter = require('./orderRouter')
-
-app.use(express.json())
-app.use(orderRouter)
-app.listen(5000, ()=>{
-    console.log('listening');
-})
